7fad7b66c7c15ab64b7d03e849043ae01382a4b7
Isac Artzi <dr.artzi@gmail.com>
2016-07-24 21:49:42 -0700

    Project 4

---
+++ plot1.R
@@ -0,0 +1,12 @@
+# PLOT 1
+# reading relevant data
+data <- read.table("household_power_consumption.txt", sep = ";", skip = 66637, nrows = 2880)
+
+# clearing name row and unrelevant variables, creating numerics
+data <- data %>% select(V3) %>% mutate(V3 = as.numeric(as.character(V3)))
+
+# create plot 1, histogram, and save to file
+png("plot1.png", width=480, height=480)
+hist(data$V3, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
+
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,16 @@
+# PLOT 3
+# read data
+data <- read.table("household_power_consumption.txt", sep = ";", skip = 66637, nrows = 2880)
+
+# clean data
+data <- data %>% mutate(V1 = as.POSIXct(dmy_hms(as.character(paste(V1, V2)))),V7 = as.numeric(as.character(V7)),V8 = as.numeric(as.character(V8)),V9 = as.numeric(as.character(V9))) %>% select(V1,V7:V9)
+
+# create plot3 and save to file
+png(filename="plot3.png")
+with(data, plot(V1,V7, type="n", xlab = "", ylab = "Energy Sub Metering"))
+with(data, points(V1,V7, col="black", type="l"))
+with(data, points(V1,V8, col="red", type="l"))
+with(data, points(V1,V9, col="blue", type="l"))
+theLegend <- c("Sub_Metering_1", "Sub_Metering_2", "Sub_Metering_3")
+legend("topright", lty=1, col = c("black", "red", "blue"),legend = theLegend)
+dev.off()
